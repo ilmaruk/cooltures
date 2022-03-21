74eafb4101977c26116dbff54a00f6a26eac97f9
ilmaruk-jt <ruggero.ferretti@jumptech.co.uk>
2022-03-21 17:35:35 +0100

    Names: simplified English.

--- names/generator.go
+++ names/generator.go
@@ -14,28 +14,38 @@ const (
 func getGenerator(c Culture, r cooltures.Randomiser) Generator {
 	switch c {
 	case CultureEnglish:
-		return englishGenerator{rand: r}
+		return simpleGenerator{
+			firstNames: engFirstNames,
+			lastNames:  engLastNames,
+			rand:       r,
+		}
 	default:
-		return simpleGenerator{rand: r}
+		return simpleGenerator{
+			firstNames: map[Gender][]string{
+				GenderFemale: {"Jane"},
+				GenderMale:   {"Joe"},
+			},
+			lastNames: []string{"Bloggs"},
+			rand:      r,
+		}
 	}
 }
 
 type simpleGenerator struct {
-	rand cooltures.Randomiser
+	firstNames map[Gender][]string
+	lastNames  []string
+	rand       cooltures.Randomiser
+}
+
+func (s simpleGenerator) FullName(g Gender) string {
+	return s.FirstName(g) + " " + s.LastName()
 }
 
 func (s simpleGenerator) FirstName(g Gender) string {
-	switch g {
-	case GenderFemale:
-		return "Jane"
-	case GenderMale:
-		return "Joe"
-	default:
-		names := []string{"Jane", "Joe"}
-		return names[s.rand.Intn(len(names))]
-	}
+	g = pickGender(g, s.rand)
+	return pickName(s.firstNames[g], s.rand)
 }
 
 func (s simpleGenerator) LastName() string {
-	return "Bloggs"
+	return pickName(s.lastNames, s.rand)
 }
--- names/names_test.go
+++ names/names_test.go
@@ -9,7 +9,6 @@ import (
 
 func TestFullName(t *testing.T) {
 	tt := []struct {
-		culture  names.Culture
 		gender   names.Gender
 		expected string
 	}{
@@ -21,24 +20,13 @@ func TestFullName(t *testing.T) {
 			gender:   names.GenderMale,
 			expected: "Joe Bloggs",
 		},
-		{
-			culture:  names.CultureEnglish,
-			gender:   names.GenderFemale,
-			expected: "Evelyn Turpin",
-		},
-		{
-			culture:  names.CultureEnglish,
-			gender:   names.GenderMale,
-			expected: "Aiden Turpin",
-		},
 	}
 
 	n := names.New()
 	for _, tc := range tt {
-		t.Run(fmt.Sprintf("Culture %s / Gender %s", tc.culture, tc.gender), func(t *testing.T) {
+		t.Run(fmt.Sprintf("Gender %s", tc.gender), func(t *testing.T) {
 			o := names.Options{
-				Culture: tc.culture,
-				Gender:  tc.gender,
+				Gender: tc.gender,
 			}
 			assert.Equal(t, tc.expected, n.FullName(o))
 		})
